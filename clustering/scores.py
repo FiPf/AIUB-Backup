f704bc7ff06505bc6cd7691930f5f30647338117
FiPf <121694520+FiPf@users.noreply.github.com>
2025-03-03 09:57:11 +0100

    collected links for different scores

---
+++ clustering/scores.py
@@ -0,0 +1,8 @@
+#https://scikit-learn.org/stable/modules/generated/sklearn.metrics.davies_bouldin_score.html
+#https://en.wikipedia.org/wiki/Davies%E2%80%93Bouldin_index 
+
+#https://en.wikipedia.org/wiki/Calinski%E2%80%93Harabasz_index
+#https://scikit-learn.org/stable/modules/generated/sklearn.metrics.calinski_harabasz_score.html 
+
+#https://en.wikipedia.org/wiki/Dunn_index
+#https://www.geeksforgeeks.org/dunn-index-and-db-index-cluster-validity-indices-set-1/ 
