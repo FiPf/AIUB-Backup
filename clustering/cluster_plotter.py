9e8640df8aa577d38b498197a138766426a9b1b0
FiPf <121694520+FiPf@users.noreply.github.com>
2025-09-11 07:42:49 +0200

    increasing image resolution for IAC plots

--- clustering/cluster_plotter.py
+++ clustering/cluster_plotter.py
@@ -124,7 +124,7 @@ class ClusterPlotter:
         # Save or show
         if save_name is not None:
             unique_save_path = self.save_unique_plot(save_name, os.path.dirname(save_name))
-            plt.savefig(unique_save_path, dpi=300, bbox_inches='tight')
+            plt.savefig(unique_save_path, dpi=1000, bbox_inches='tight')
             print(f"Plot saved as: {unique_save_path}")
             plt.close()
         else:
